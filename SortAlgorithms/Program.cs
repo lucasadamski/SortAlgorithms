c0fb4c634ae31ddaf7fd13e84e704c0525806966
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2023-12-01 10:05:46 +0000

    Feat Bubble Sort

--- SortAlgorithms/Program.cs
+++ SortAlgorithms/Program.cs
@@ -10,6 +10,11 @@ namespace SortAlgorithms
 
             InnerSort(array);
             PrintArray(array);
+
+            array = new int[] { 6, 8, 2, 4, 6, 9, 2, 4, 0, 1 };
+            BubbleSort(array);
+            PrintArray(array);
+
         }
         static void InnerSort(int[] tab)
         {
@@ -26,12 +31,29 @@ namespace SortAlgorithms
             }
         }
 
+        static void BubbleSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j >= i; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            }
+        }
+
         static void PrintArray(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(array[i]);
             }
+            Console.Write("\n");
         }
     }
 }
